56c09510969bab41034c78c011ab978e293646bb
Lawrence Moraira <lawrence@qburst.com>
2015-05-27 17:55:15 +0530

    Added Gallery

--- js/bikes.js
+++ js/bikes.js
@@ -1,18 +1,8 @@
 $(document).ready(function() {
-	var $option = $('#sel_bikes');
-  	$.getJSON('assets/json/bikes.JSON', function(data) {
-	    $.each(data.bikes,function(key,value) {
-    		$option.append('<option>' + value.label + '</option>');
-    	});   
-  
-    $('#sel_bikes').on('change', function(){
-    	for(var i = 0; i<data.bikes.length; i++) {
-    		if(data.bikes[i].label == $(this).val()) {
-        		$("#bike-img").html('<img src="'+data.bikes[i].value+'" alt="Bike Image">');
-         	}
-        }
-    });
+	var $container = $('#gallery-container');
+  $.getJSON('assets/json/bikes.JSON', function(data) {
+	  $.each(data.bikes,function(key,value) {
+      $container.append('<img class="gallery" src="'+ value.value +'">');
+    });   
   });
 });  
-
-
