1dc105445bc1fdde50aa914e091b1071e4d94a9a
Lawrence Moraira <lawrence@qburst.com>
2015-05-27 10:51:39 +0530

    Responsive to 1980px

--- js/home.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  var $content = $('#main-content');
-  $.getJSON('assets/json/data.JSON',function(data) {
-    $.each(data.data,function(key,value) {
-      $content.append('<div class="content">' + value.label + '</div>');
-    });
-  });
-});
