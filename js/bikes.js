cb95468a4b7260fe98dfcecff8eb11fcf2db78f7
Lawrence Moraira <lawrence@qburst.com>
2015-05-28 17:54:19 +0530

    Slider

--- js/bikes.js
+++ js/bikes.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
+  $(".fancybox").fancybox();
 	var $container = $('#gallery-container');
+  var $content = $('.content');
   $.getJSON('assets/json/bikes.JSON', function(data) {
 	  $.each(data.bikes,function(key,value) {
-      $container.append('<img class="gallery" src="'+ value.value +'">');
+      $container.append('<div class="gallery"><a class="fancybox" rel="group" href="#'+value.id+'"><img src="'+ value.value +'"></a><span class="caption">'+value.label+'</span></div>');
+      $content.append('<div id="'+value.id+'"><h1>'+value.label+'</h1><img class="imgslider" src="'+ value.value +'"></div>')
     });   
   });
 });  
