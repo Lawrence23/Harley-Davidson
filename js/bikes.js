0ced64e529678072a66b5171271c5ac068c74219
Lawrence Moraira <lawrence@qburst.com>
2015-06-08 10:46:50 +0530

    Added Keyboard Navigation

--- js/bikes.js
+++ js/bikes.js
@@ -14,6 +14,7 @@ function openPopup(obj,index) {
   $bikecontent.html('<div id="'+obj[index].imgId+'" class="bike-content"><h1>'+obj[index].label+'</h1><a href = "javascript:void(0)" style="float:right;"><img id="close" src="assets/images/close.png"></a><a href = "javascript:void(0)" ><img id="prev" src="assets/images/prev.png"></a><a href = "javascript:void(0)"><img id="next" src="assets/images/next.png"></a><img class="imgslider" src="'+ obj[index].value +'"><table><tr><th colspan="2">PRICING</th></tr><tr><td>Vivid Black</td><td>'+obj[index].price+'</td></tr><tr><th colspan="2">DIMENSIONS</th></tr><tr><td>Length</td><td>'+obj[index].length+'</td></tr><tr><td>Seat Height</td><td>'+obj[index].height+'</td></tr><tr><td>Ground Clearance</td><td>'+obj[index].clearance+'</td></tr><tr><td>Fuel Capacity</td><td>'+obj[index].fuel+'</td></tr><tr><td>Weight</td><td>'+obj[index].weight+'</td></tr><tr><th colspan="2">ENGINE</th></tr><tr><td>Engine</td><td>'+obj[index].engine+'</td></tr><tr><td>Displacement</td><td>'+obj[index].disp+'</td></tr><tr><td>Compression Ratio</td><td>'+obj[index].ratio+'</td></tr><tr><th colspan="2">PERFORMANCE</th></tr><tr><td>Engine Torque</td><td>'+obj[index].torque+'</td></tr><tr><td>Engine Torque(rpm)</td><td>'+obj[index].rpm+'</td></tr><tr><td>Fuel Economy</td><td>'+obj[index].eco+'</td></tr></table></div>');  
   
   document.getElementById('close').onclick = function () { closePopup(obj,index); }
+  document.getElementById('fade').onclick = function () { closePopup(obj,index); }
   document.getElementById('prev').onclick = function () { openPopup(obj,$prev); }
   document.getElementById('next').onclick = function () { openPopup(obj,$next); }
 
@@ -27,7 +28,9 @@ function openPopup(obj,index) {
                 break;
       case 38:
       case 39:  openPopup(obj,$next);
-                break;  
+                break;
+      case 27:  closePopup(obj,index);
+                break;           
     }  
   }
 
@@ -35,7 +38,16 @@ function openPopup(obj,index) {
 
 function closePopup(obj,index) {
   document.getElementById(obj[index].imgId).style.display='none';
-  document.getElementById('fade').style.display='none';  
+  document.getElementById('fade').style.display='none';
+  document.onkeydown = function(e) {
+    switch(e.keyCode) {
+      case 27:
+      case 37:
+      case 40:  
+      case 38:
+      case 39:  break;  
+    }  
+  }  
 }
 
 
@@ -45,7 +57,7 @@ $(document).ready(function() {
   $.getJSON('assets/json/bikes.JSON', function(data) {
     var bike = data.bikes;
     $.each(data.bikes,function(key,value) {
-      $container.append('<div class="gallery"><a  href = "javascript:void(0)"><img id="'+value.id+'" src="'+ value.value +'"></a><span class="caption">'+value.label+'</span></div>');
+      $container.append('<div class="gallery"><a  href = "javascript:void(0)"><img id="'+value.id+'" src="'+ value.value +'"><span class="caption">'+value.label+'</span></a></div>');
       document.getElementById(value.id).onclick = function () { openPopup(bike,key); }
     });
   });
